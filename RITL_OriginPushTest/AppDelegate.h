10b2c7ce4edc71de8225e30f18758be77e1ae9c8
774655048@qq.com <774655048@qq.com>
2016-09-23 10:44:38 +0800

    Initial Commit

---
+++ RITL_OriginPushTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RITL_OriginPushTest
+//
+//  Created by YueWen on 2016/9/23.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RITL_OriginPushTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITL_OriginPushTest
+//
+//  Created by YueWen on 2016/9/23.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
